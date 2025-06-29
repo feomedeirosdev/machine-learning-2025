49af4e4c4cd114625d9ea72aaf7593cf08312dfb
Fabio Medeiros <feomedeirosdev@gmail.com>
2025-06-29 16:27:57 -0300

    adicionando dados

---
+++ comunidade.py
@@ -0,0 +1,9 @@
+# %%
+import pandas as pd
+
+# %%
+data_path = '/home/medeiros/Área de trabalho/TrilhaML/9_Machine_Learning_2025/machine-learning-2025/datas/Dados Comunidade (respostas) - dados.csv'
+df = pd.read_csv(data_path)
+df.head()
+
+# %%
